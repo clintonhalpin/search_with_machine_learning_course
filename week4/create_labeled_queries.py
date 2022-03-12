ca4147d4c9de2bd56328aebadee8f443d77ac620
Clinton Halpin <clintonhalpin@gmail.com>
2022-03-12 16:27:18 +0000

    feat: update create_labeled_queries, build qu model

--- week4/create_labeled_queries.py
+++ week4/create_labeled_queries.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
 import csv
+import utilities.functions as fn
 
 # Useful if you want to perform stemming.
 import nltk
@@ -17,6 +18,7 @@ output_file_name = r'/workspace/datasets/labeled_query_data.txt'
 parser = argparse.ArgumentParser(description='Process arguments.')
 general = parser.add_argument_group("general")
 general.add_argument("--min_queries", default=1,  help="The minimum number of queries per category label (default is 1)")
+general.add_argument("--sample_size", default=1000000,  help="Queries to sample from")
 general.add_argument("--output", default=output_file_name, help="the file to output to")
 
 args = parser.parse_args()
@@ -24,6 +26,9 @@ output_file_name = args.output
 
 if args.min_queries:
     min_queries = int(args.min_queries)
+
+if args.sample_size:
+    sample_size = int(args.sample_size)    
 
 # The root category, named Best Buy with id cat00000, doesn't have a parent.
 root_category_id = 'cat00000'
@@ -46,16 +51,48 @@ parents_df = pd.DataFrame(list(zip(categories, parents)), columns =['category', 
 
 # Read the training data into pandas, only keeping queries with non-root categories in our category tree.
 df = pd.read_csv(queries_file_name)[['category', 'query']]
+df = df.sample(n=sample_size)
 df = df[df['category'].isin(categories)]
 
 # IMPLEMENT ME: Convert queries to lowercase, and optionally implement other normalization, like stemming.
+print('> cleaning_queries')
+df['clean_query'] = df['query'].apply(fn.clean_query)
+
+#
+# Get Size of Category
+#
+cat_value_counts = df['category'].value_counts()
+def get_category_size(category):
+    return cat_value_counts[category]
+# 
+# Given a frame and category fetch the parent
+# 
+def get_parent_category(frame, category):
+    return frame[frame['category'] == category]['parent'].item();    
+
+# 
+# Check for min queries and roll up
+#
+def roll_up_category(cat):
+    cat_size = get_category_size(cat);
+    if cat_size < min_queries:
+        parent_cat = get_parent_category(parents_df, cat);
+        return parent_cat
+    return cat
+    
 
 # IMPLEMENT ME: Roll up categories to ancestors to satisfy the minimum number of queries per category.
+print('> checking for min_queries');
+print("> original unique categories={0}".format(df['category'].value_counts().size))
+df['category'] = df['category'].apply(roll_up_category)
 
 # Create labels in fastText format.
 df['label'] = '__label__' + df['category']
 
 # Output labeled query data as a space-separated file, making sure that every category is in the taxonomy.
 df = df[df['category'].isin(categories)]
-df['output'] = df['label'] + ' ' + df['query']
+df['output'] = df['label'] + ' ' + df['clean_query']
 df[['output']].to_csv(output_file_name, header=False, sep='|', escapechar='\\', quoting=csv.QUOTE_NONE, index=False)
+
+
+print("> final categories={0}".format(df['category'].value_counts().size))
